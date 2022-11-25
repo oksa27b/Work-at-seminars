a2693b1e680457adf342bf492b8bdcef686c7321
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-11-25 10:57:49 +0300

    /Seminar_4_3/Program.cs

---
+++ Seminar_4_3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+//  заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+// int [] GetArray(int  size)
+// {
+//    int [] array1 = new int[size];
+
+//    for (int i = 0; i < size; i++)
+//    {
+//     array1[i] = new Random().Next(0,2);
+//    }
+    
+//     return array1;
+
+// }
+// int [] array = GetArray(8);
+
+// Console.Write(array);
+
+int [] GetArray(int [] array)
+{
+   
+   for (int i = 0; i <array.Length; i++)
+   {
+    array[i] = new Random().Next(0,2);
+   }
+    
+    return array;
+
+}
+// сначало 00000000 мы обращаемся к методу чтобы выполнить какое-то действие
+int [] array = new int [8];
+ GetArray(array);
+
+Console.Write(String.Join(",", array));
+
