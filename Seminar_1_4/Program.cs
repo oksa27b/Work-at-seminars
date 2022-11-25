2617fcd7011a1b767b192153e8c2c687f93e0108
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-11-25 10:18:09 +0300

    /Seminar_1_4/Program.cs

---
+++ Seminar_1_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке -N до N.
+
+// 4 ->"-4, -3,-2, -1, 0, 1, 2, 3, 4"
+// 2 ->"-2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+
+int negNum=- num;
+
+while(negNum<=num)
+{
+    Console.WriteLine(negNum);
+    // Console.Write(negNum);
+    negNum++;
+}
