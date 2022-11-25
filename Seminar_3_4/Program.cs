c8bdc00b8aa535d8152b6de0d9b31055bb23c0fa
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-11-25 10:53:08 +0300

    /Seminar_3_4/Program.cs

---
+++ Seminar_3_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру
+//  заданного числа или сообщает, что третьей цифры нет.
+
+int  num = new Random ().Next(100,100000);
+Console.WriteLine(num);
+int result =0;
+while(result==0)
+{
+    if (num>99&&num<1000)
+    {
+        result= num %10;
+    }
+    num/=10;
+
+}
+Console.WriteLine(result);
