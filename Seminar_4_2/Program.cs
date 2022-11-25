06d97fc770f4eaa8e437f3cf9c7ca1c9e0e524ce
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-11-25 10:56:33 +0300

    /Seminar_4_2/Program.cs

---
+++ Seminar_4_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N
+//  и выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24 
+// 5 -> 120
+
+int GetCounNum(int res)
+{
+   
+    int count =1;
+    int result = 1;
+
+    while(res>=count)
+    {
+        
+        result*=count;
+        count++;
+
+    }
+    return result;
+
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Произведение "+GetCounNum(num));
