5281b2e8f03acfa15206c40e4ef1d82a3df5b59c
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-11-25 10:27:43 +0300

    /Seminar_2_3/Program.cs

---
+++ Seminar_2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+int num1 = new Random().Next(10,100);
+int num2 = new Random().Next(10,100);
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+
+if((num2==num1*num1)||(num1==num2*num2))
+{
+Console.Write("ДА");
+}
+else
+{
+Console.Write("НЕТ");
+}
