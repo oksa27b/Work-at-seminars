ac6560988d320e4867493d7560a01946873cd77e
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-11-25 10:28:39 +0300

    /Seminar_2_4/Program.cs

---
+++ Seminar_2_4/Program.cs
@@ -0,0 +1,19 @@
+﻿int num = 5*5*5;
+
+double num2 = Math.Pow(5,3);
+
+Console.ReadLine();
+
+Console.WriteLine (num2);
+
+
+// double (более точная переменная до 16 знаков)
+double num = Math.Pow(5.3,4);
+
+
+
+
+int num = 6;
+Console.WriteLine("Число "+ num); 
+// или
+Console.WriteLine($"Число {num}");
