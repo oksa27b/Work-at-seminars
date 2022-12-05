d88cfa60c805dd2f9832f1a00e223b4c537288d5
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-12-06 01:12:50 +0300

    /Seminar_6_2/Program.cs

---
+++ Seminar_6_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+//  одномерный массив (последний элемент будет на первом
+//   месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+// Console.Write(" N =  ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+int []array = new int [6];
+
+int [] GetArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1,10);
+        
+    }
+    return array;
+}
+int [] ChangeArray(int[] array)
+{
+    for (int i = 0; i <  array.Length/2; i++)
+    {
+      
+        int temp = array[i];
+        array[i] = array[array.Length-i-1];
+        array[array.Length-i-1] = temp;
+
+    }
+    return array;
+}
+Console.WriteLine(String.Join(",",GetArray(array)));
+Console.WriteLine();
+Console.WriteLine(String.Join(",",ChangeArray(array)));
