24d775cc179c24c962b0c8076c9fa010f248db7c
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-12-06 01:16:33 +0300

    /Seminar_7/Program.cs

---
+++ Seminar_7/Program.cs
@@ -0,0 +1,49 @@
+﻿// ДЗ
+
+
+Console.Write("Сколько чисел вы хотите ввести? ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [numb];
+
+int PositiveCount(int count)
+{
+    int countOfPositive = 0;
+    for(int i = 0; i < count;i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0)
+        {
+            countOfPositive+=1;
+        }
+        Console.WriteLine(string.Join(",", array));
+    }
+    return countOfPositive;
+}
+int posCount = PositiveCount(numb);
+Console.Write($"количество положительных чисел = {posCount}");
+
+
+
+// int totalNegativeNumbers = 0;
+// bool b = true;
+// while (b)
+// {
+//     Console.Write("Хотите ввести число? Если  ДА наберите  1: ");
+
+//     int n = Convert.ToInt32(Console.ReadLine());
+
+//     if (n == 1)
+//     {
+//         Console.Write("Введите число: ");
+//         int m = Convert.ToInt32(Console.ReadLine());
+//         if (m > 0)
+//         {
+//             totalNegativeNumbers = totalNegativeNumbers + 1;
+//         }
+//     }
+//     else
+//     {
+//         Console.Write($"Спасибо! Чисел больше 0 введено {totalNegativeNumbers} раз");
+//         b = false;
+//     }
+// }
