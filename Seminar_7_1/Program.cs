a7033219c7d1b03480a8c7d2fe60e2c58ec85f2f
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-12-06 01:18:02 +0300

    /Seminar_7_1/Program.cs

---
+++ Seminar_7_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 46: Задайте двумерный массив размером
+//  m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Write("Введите кол-во столбцов ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите кол-во строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+
+
+int [,] matrix = new int[rows,columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] =new Random().Next(0,12);
+        Console.Write(matrix[i,j]+" ");
+    
+    }
+    Console.WriteLine();
+}
+
+
+
+
