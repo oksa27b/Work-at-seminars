9963baabcfbe5e6c999804d509c65f7a22736f08
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-12-06 01:20:38 +0300

    /Seminar_7_2/Program.cs

---
+++ Seminar_7_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 48: Задайте двумерный массив размера
+//  m на n, каждый элемент в массиве находится 
+//  по формуле: Aₘₙ = m+n. Выведите полученный
+//   массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Write("Введите кол-во столбцов ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите кол-во строк ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+
+int [,] matrix = new int[rows,columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] =i+j;
+        Console.Write(matrix[i,j]+" ");
+    
+    }
+    Console.WriteLine();
+}
+
