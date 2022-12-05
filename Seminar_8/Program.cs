6d1957e9e0a1466c3981db4cb803813c70f268f0
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-12-06 01:27:59 +0300

    /Seminar_8/Program.cs

---
+++ Seminar_8/Program.cs
@@ -0,0 +1,35 @@
+﻿// ДЗ
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] matrix = new int[new Random().Next(3, 5), new Random().Next(3, 5)];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0, 12);
+        Console.Write(matrix[i, j] + " ");
+
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("_______________");
+
+double sum = 0;
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    double average = Math.Round(sum/matrix.GetLength(0), 2);
+    sum = 0;
+    Console.Write(average+ " ");
+}
+
