ac3cb5ca053855677d26ac3dabab8d50a2105ac1
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-12-06 01:35:08 +0300

    /Seminar_9_2/Program.cs

---
+++ Seminar_9_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 67: Напишите программу, которая 
+// будет принимать на вход число и возвращать
+//  сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите начальное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(SumNum(n));
+
+int SumNum(int n)
+{
+    if (n==0)
+    {
+        return 0;
+    }
+    return(n%10+SumNum(n/10));
+   
+}
