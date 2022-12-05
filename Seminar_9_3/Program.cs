ad8860e8906047bbf5fd647e7fabaffbd503ad29
oksa27b <118002320+oksa27b@users.noreply.github.com>
2022-12-06 01:37:33 +0300

    /Seminar_9_3/Program.cs

---
+++ Seminar_9_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 69: Напишите программу, которая на вход 
+// принимает два числа A и B, и возводит число А в
+//  целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите начальное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(Pow(n,m));
+
+int Pow(int n,int m)
+{
+    if (m==0)
+    {
+        return 1;
+    }
+     if (m==1)
+    {
+        return n;
+    }
+    return(n*Pow(n,m-1));
+   
+}
+
+
